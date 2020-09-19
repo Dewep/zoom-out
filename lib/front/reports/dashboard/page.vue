307d8141edcf8fc789069861a68a1d0319fe8208
Maigret Aurélien <dewep@pandalab.fr>
2020-09-19 10:18:50 +0200

    Base override front

---
+++ lib/front/reports/dashboard/page.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="about">
+    <h1>Dashboard</h1>
+
+    <component
+      :is="componentName"
+      v-for="componentName in componentNames"
+      :key="componentName"
+    />
+  </div>
+</template>
+
+<script>
+import reports from '@/reports/vue'
+
+export default {
+  name: 'ReportDashboardPage',
+
+  components: {
+    ...reports.dashboard.components
+  },
+
+  computed: {
+    componentNames () {
+      return reports.dashboard.names
+    }
+  }
+}
+</script>
--- vue.config.js
+++ vue.config.js
@@ -1,10 +1,33 @@
+const AliasPlugin = require('enhanced-resolve/lib/AliasPlugin')
 const path = require('path')
 
 module.exports = {
   productionSourceMap: false,
 
-  chainWebpack: config => {
-    config.resolve.alias
-      .set('@', path.resolve(__dirname))
+  configureWebpack: config => {
+    if (!config.resolve) {
+      config.resolve = {}
+    }
+    if (!config.resolve.plugins) {
+      config.resolve.plugins = []
+    }
+    if (config.resolve.alias && config.resolve.alias['@']) {
+      delete config.resolve.alias['@']
+    }
+    config.resolve.plugins.push(
+      new AliasPlugin(
+        'described-resolve',
+        [
+          {
+            name: '@',
+            alias: [
+              path.resolve(__dirname),
+              path.resolve(__dirname, 'lib', 'front')
+            ]
+          }
+        ],
+        'resolve'
+      )
+    )
   }
 }
