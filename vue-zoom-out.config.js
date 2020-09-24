81e934bff67d94e820120d3ad686c119c40bbf2d
Maigret Aurélien <dewep@pandalab.fr>
2020-09-24 16:44:31 +0200

    Update vue-config

---
+++ vue-zoom-out.config.js
@@ -0,0 +1,68 @@
+const AliasPlugin = require('enhanced-resolve/lib/AliasPlugin')
+const path = require('path')
+
+const zoomOutRootPathDefault = path.resolve(__dirname, 'lib', 'front')
+
+module.exports = function ({
+  projectRootPath = null,
+  zoomOutRootPath = zoomOutRootPathDefault,
+  chainWebpackExtra = null,
+  configureWebpackExtra = null,
+  devServer = null
+}) {
+  const vueConfig = {}
+
+  vueConfig.productionSourceMap = false
+
+  if (devServer) {
+    vueConfig.devServer = devServer
+  }
+
+  vueConfig.css = {
+    loaderOptions: {
+      sass: {
+        sassOptions: {
+          includePaths: [
+            path.join(zoomOutRootPath, 'styles')
+          ]
+        }
+      }
+    }
+  }
+  if (projectRootPath) {
+    vueConfig.css.loaderOptions.sass.includePaths.unshift(path.join(projectRootPath, 'styles'))
+  }
+
+  vueConfig.chainWebpack = function (config) {
+    config.entry('main').clear()
+    config.entry('main').add(path.join(zoomOutRootPath, 'main.js'))
+
+    if (chainWebpackExtra) {
+      chainWebpackExtra(config)
+    }
+  }
+
+  vueConfig.configureWebpack = function (config) {
+    if (!config.resolve) {
+      config.resolve = {}
+    }
+    if (!config.resolve.plugins) {
+      config.resolve.plugins = []
+    }
+    if (config.resolve.alias && config.resolve.alias['@']) {
+      delete config.resolve.alias['@']
+    }
+    const alias = [zoomOutRootPath]
+    if (projectRootPath) {
+      alias.unshift(projectRootPath)
+    }
+    const aliasPlugin = new AliasPlugin('described-resolve', [{ name: '@', alias }], 'resolve')
+    config.resolve.plugins.push(aliasPlugin)
+
+    if (configureWebpackExtra) {
+      configureWebpackExtra(config)
+    }
+  }
+
+  return vueConfig
+}
