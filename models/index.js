24b2142a9efc57dd1085f902678f853219e6dfbf
Maigret Aurélien <dewep@pandalab.fr>
2020-09-24 16:44:12 +0200

    Remove project information

--- models/index.js
+++
@@ -1,4 +0,0 @@
-module.exports = [
-  require('./message'),
-  require('./conference')
-]
--- reports/requests/page.vue
+++
@@ -1,5 +0,0 @@
-<template>
-  <div class="about">
-    <h1>TOTOzzzzzzzzzzzzzzzzzzzz</h1>
-  </div>
-</template>
